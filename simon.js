8ffeba1c0524d0bde9fbec58ee554eef25c4cf28
jason goldfarb <jasongoldfarb@jasons-MacBook-Pro.local>
2016-03-02 13:55:14 -0700

    added animate and randomnum sections

--- simon.js
+++ simon.js
@@ -1,147 +1,114 @@
-$(document).ready(function() {
-  'use strict'
-  var game  ={
+
+define(['jquery'], function($))
+  'use strict';
+// var Hal is the variable that has the enterty of the code init
+//set up initial variables an array to contain hals choice HalSequence,
+//player sequence playerMatch, initation of the rounds at 0.set the variable
+//active to true and mode normal
+  var Hal = {
     halSequence: [],
-    playerSequence: [],
-    round: 1,
-    active: true;
+    playerMatch: [],
+    round: 0,
+    active: true,
+    mode: 'normal'
 
-    // colorChoice: {1:'red', 2: 'blue', 3: 'green', 4: 'yellow'}
-    // active: false,
-  }
-  //function init is to initiate the game by calling halStart that resets all the
-  //base parameters
-  init: function() {
+//This function will initialize the game by clicking the Press to play
+//button it is called by the attribute in brackets clicking
+// start will call the startGame function by setting a local variable
+// that to this will allow for less confusion when initializing the
+// game with this
+  init: function(){
     var that = this;
-     $('.start').on('click', function() {
-       that.halStart();
-     });
-  },
+     $('[data-action.start]').on('click', function() {
+       console.log("hello World");
+       that.startGame();
+      })
+     };
+// the starGame function sets the  initial parameters for
+//game resets the empty arrays and round starts at 0 makes
+//sure active is true
 
-  halStart: function(){
-    this.halSequence = [];
-    this.playerSequence = [];
-    this.round = 1;
-    this.active = true;
-// this hides the lossing message
-  $('p[data-action="lose"]').hide();
-  //calls the new round function
-  this.newRound();
+  startGame: function(){
+       this.halSequence = [];
+       this.playerSequence = [];
+       this.round = 0;
+       this.active = true;
+     }
+//This function new round does a jquery search for the
+//data-round attribute the initial number is always 0 and the
+//++this.roundwill add one to the roundwill
 
-},
+//the this.halSequence is called and we want to have Hal
+//make a choice this is accomplished by using math random
+//function to make a choice between the div's and pushes
+//and stores it into the array.
+// the playerMatch is then set equal to the halSequence with the index [0
+//] in the array sliced out  then the this.halSequence is called by the animate function
+
+  newRound: function() {
+       $('[data-round]').text(++this.round);
+        this.halSequence.push(this.randomNumber());
+        this.playerSequence = this.halSequence.slice(0);
+        this.animate(this.halSequence);
+      },
+})
+// }
+//do not fuck with code above!!
 //add a new color sequence and animate's it to the user
-newRound: function(e) {
-  //shift() will remove the [0] element from the playerSequence
-  var desiredReply = this.playerSequence.shift();
-  var actualReply = $(e.target).data('tile');
-  this.active = (disiredResponse === actualResponse);
-  this.halWins();
-},
 
-halWins: function() {
-  if (this.playSequence.length === 0 && this.active) {
-    this.deactivateHalBoard();
-    this.newRound();
+// helperfunctions
 
-  } else if (!this.active){
-    this.deactivateHalBoard();
-    this.endGame();
+var randomNumber = function() {
+  //return random between 1 and four
+  return Math.floor((Math.random()*4)+1);
   }
-},
-  endGame: function() {
-    //tells the player they lost and resets "round" text to 0
-      $('p[data-action="lose"]').show();
-      $($('[data-round]').get(0)).text('0');
-  },
+};
+//animate function is passed the halSequence array
+//
+animate: function(halSequence) {
+  var i = 0;
+  var that = this;
+var interval = setInterval(function() {
 
-  /*----------------- helper functions -------------*/
-//prevents user from playing
-deactivateHalBoard(): function() {
-if(this.mode !== 'freeboard') {
-  $('hal')
-    .off('click','[data-title]')
-    .off('mousedown','[data-title]')
-    .off('mouseup','[data-title]');
 
-  	$('[data-tile]').removeClass('hoverable');
-}
+})
+
 }
 
-  // var $active = false;
-  //   event.preventDefault();
-  //   console.log("start game");
-  //   $active = true;
-  //   checkStatus();
-  // });
-  //
-  // function checkStatus(){
-  //   if($active){
-  //     console.log("You can start to play")
-  //
-  //   }
-  //
-  // }
-  //
-  //
-  // // var mathRandom = Math.random()
-  // // if (mathRandom  {
-  //
-  // function halPick(){
-  //
-  //   for(i=0; i < round; i++) {
-  //   this.arrHal.push(Math.floor(Math.random()*4)+1);
 
 
-    // }
+// this hides the lossing message
+  // $('p[data-action="lose"]').hide();
+  //calls the new round function
+  // this.newRound();
 
-    //   halPick()
-    // };
-    // console.log(halPick());
-  })
-  // });
+// }
 
-  //step one Check to see what state the game is in
-  // function stateOfPlay() {
-
-  // };
-
-  // first thing is to make a game object
-  //     var game ={
-  //       level:1,
-  //       turn:0,
-  //       difficulty:0,
-  //       score:0,
-  //       active:false,
-  //       handler:false,
-  //       shape:'.shape'
-  //       //need two arrays one for computer one for user
-  //       computerSequence: [];
-  //       playerSequence: [],
-  //
-  //     }
-  //   }
-  //
-  //   })
-  //
-  //
-  // }
-
-
-
-  //have to have a way the user knows the computer has done something
-  //have to have a way to know what state the game is pushed button or not
-
-  //make a function to make the computer randomly select a pad
-
-
-  // after selection is made put computer choice in the computerSequence array;
-
-
-  //selection will be displayed for the player.
-
-  //now the user makes a choice click and the that is pushed into the playerSequence array;
-
-  //now use a switch statment to determine if the computerSequence and playerSequence are
-  //equivalent.
-  //if the two are equivalent then return to the randomizePad function to add to the index array
-  // to computer
+// halWins: function() {
+//   if (this.playSequence.length === 0 && this.active) {
+//     this.deactivateHalBoard();
+//     this.newRound();
+//
+//   } else if (!this.active){
+//     this.deactivateHalBoard();
+//     this.endGame();
+//   }
+// },
+//   endGame: function() {
+//     //tells the player they lost and resets "round" text to 0
+//       $('p[data-action="lose"]').show();
+//       $($('[data-round]').get(0)).text('0');
+//   },
+//
+//   /*----------------- helper functions -------------*/
+// //prevents user from playing
+// deactivateHalBoard(): function() {
+// if(this.mode !== 'freeboard') {
+//   $('hal')
+//     .off('click','[data-title]')
+//     .off('mousedown','[data-title]')
+//     .off('mouseup','[data-title]');
+//
+//   	$('[data-tile]').removeClass('hoverable');
+ // };
+// });
